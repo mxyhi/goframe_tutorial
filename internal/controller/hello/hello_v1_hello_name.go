ec8074d9818471e1ba704531339ec1e2987bef8d
m <2528988582@qq.com>
2024-06-10 12:54:33 +0800

    feat: fix hello name api endpoint

--- internal/controller/hello/hello_v1_hello_name.go
+++ internal/controller/hello/hello_v1_hello_name.go
@@ -3,12 +3,12 @@ package hello
 import (
 	"context"
 
-	"github.com/gogf/gf/v2/errors/gcode"
-	"github.com/gogf/gf/v2/errors/gerror"
+	"github.com/gogf/gf/v2/frame/g"
 
 	v1 "goframe_tutorial/api/hello/v1"
 )
 
 func (c *ControllerV1) HelloName(ctx context.Context, req *v1.HelloNameReq) (res *v1.HelloNameRes, err error) {
-	return nil, gerror.NewCode(gcode.CodeNotImplemented)
+	g.RequestFromCtx(ctx).Response.Writeln("Hello " + req.Name)
+	return
 }
